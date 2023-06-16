b1900e7727b7b5210172ad7045392bd50ed61f47
Spencer <sjk7876@rit.edu>
2023-06-16 15:05:53 -0400

    finished test

--- strength.py
+++ strength.py
@@ -1,9 +1,27 @@
+SYMBOLS_LIST = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+
 def main():
     # Grab input from stdin
     print("Enter the password to check here: ", end='')
     userPassword = input()
     
+    # Password Rules
+    #   No spaces
+    #   At least 14 characters
+    #   At least 2 uppercase
+    #   At least 2 lowercase
+    #   At least 2 symbols
+    #   At least 2 numbers
+    #   No dictionary words
+    #   No common passwords
     
+    score = 0
+    maxScore = 9
+    
+    for letter in userPassword:
+        print("e")
+        
     
     print(userPassword)
 
