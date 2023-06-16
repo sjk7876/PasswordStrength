11d9d5eb23a31e0cc858acf6aa0a86049642dbe7
Spencer <sjk7876@rit.edu>
2023-06-16 14:16:41 -0400

    basic input

---
+++ strength.py
@@ -0,0 +1,12 @@
+def main():
+    # Grab input from stdin
+    print("Enter the password to check here: ", end='')
+    userPassword = input()
+    
+    
+    
+    print(userPassword)
+
+
+if (__name__ == "__main__"):
+    main()
