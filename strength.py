823aa7d2d975be7e9ab9862c6c7940c390d8fa9c
Spencer <sjk7876@rit.edu>
2023-06-19 14:21:37 -0400

    can now read mulitple passwords at once

--- strength.py
+++ strength.py
@@ -1,7 +1,15 @@
 import string
 
-def checkStrength(userPassword):
-    # Password Rules
+# Global Password Strength Requirements
+NUM_UPPER = 2
+NUM_LOWER = 2
+NUM_NUMBERS = 2
+NUM_SYMBOLS = 2
+NUM_CHARACTERS = 14
+
+
+def checkStrength(userPassword, commonPasswords, commonWords):
+    # Default Password Rules
     #   No spaces
     #   At least 14 characters
     #   At least 2 uppercase
@@ -10,10 +18,11 @@ def checkStrength(userPassword):
     #   At least 2 numbers
     #   No dictionary words
     #   No common passwords
+    #   Max Length 1024 characters
     
     # Start with 0 points
-    # 10 = best
-    # 7-9 = secure
+    # 9   = best
+    # 7-8 = secure
     # 5-6 = moderate
     # 3-4 = weak
     # 0-2 = very weak
@@ -25,68 +34,82 @@ def checkStrength(userPassword):
     symbol = 0
     number = 0
     
-    f = open("100k_password.txt", "r", encoding="utf-8")
-    lines = f.read()
-    answer = lines.find(userPassword)
+    answer = commonPasswords.find(userPassword)
     if answer != -1:
         print("Password found in list of most common passwords")
         return 0
         
     answer = 0
-        
-    f = open("10k_words.txt", "r", encoding="utf-8")
-    lines = f.read()
-    answer = lines.find(userPassword)
+    
+    answer = commonWords.find(userPassword)
     if answer != -1:
         print("Password found in list of common words")
-        return 0
+        return -1
         
     
     if len(userPassword) > 1024:
         print("Password too long")
-        return 0
+        return -1
     
     elif len(userPassword) >= 14:
         length = 1
       
     for letter in userPassword:
-        if letter in string.punctuation and symbol < 2:
+        if letter in string.punctuation and symbol < NUM_SYMBOLS:
             symbol = symbol + 1
             continue
             
         if letter.isalpha:
             if letter.isdigit():
-                if number < 2:
+                if number < NUM_NUMBERS:
                     number = number + 1
                 continue
             
             if letter.isupper():
-                if uppers < 2:
+                if uppers < NUM_UPPER:
                     uppers = uppers + 1
                 continue
             
             if letter.islower():
-                if lowers < 2:
+                if lowers < NUM_LOWER:
                     lowers = lowers + 1
                 continue
     
     score = length + uppers + lowers + symbol + number
+    
+    return score
         
-    print("score:", score)
-    print("length:", length)
-    print("upper:", uppers)
-    print("lower:", lowers)
-    print("symbol:", symbol)
-    print("number:", number)
+    # print("score:", score)
+    # print("length:", length)
+    # print("upper:", uppers)
+    # print("lower:", lowers)
+    # print("symbol:", symbol)
+    # print("number:", number)
 
 
 def main():
+    passwordList = []
+    
     # Grab input from stdin
     print("Enter in a list of passwords to check, seperated by newlines and ending with 'done':")
-    userPassword = input()
+    while True:
+        password = input()
+        
+        if password == "done":
+            break
+            
+        passwordList.append(password)
     
-    print("Password:", userPassword)
-    print("Score:", checkStrength(userPassword))
+    f = open("100k_password.txt", "r", encoding="utf-8")
+    commonPasswords = f.read()
+    
+    f = open("10k_words.txt", "r", encoding="utf-8")
+    commonWords = f.read()
+    
+    for p in passwordList:
+        print("Password:", p)
+        print("Score:", checkStrength(p, commonPasswords, commonWords))
+        print()
 
 
 if (__name__ == "__main__"):
