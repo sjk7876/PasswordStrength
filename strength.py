3dfb4dabf09e4c151853605ed87639018ffb66c7
Spencer <sjk7876@rit.edu>
2023-06-19 17:10:09 -0400

    Finished verbosity parameter

--- strength.py
+++ strength.py
@@ -3,17 +3,25 @@ import argparse
 import sys
 
 
-ASCII_ART = "\
-|--------------------------------------------------------------------------|\n\
-|\t\t      _                        _   _     \t\t   |\n\
-|\t\t     | |                      | | | |    \t\t   |\n\
-|\t\t  ___| |_ _ __ ___ _ __   __ _| |_| |__  \t\t   |\n\
-|\t\t / __| __| '__/ _ \ '_ \ / _` | __| '_ \ \t\t   |\n\
-|\t\t \__ \ |_| | |  __/ | | | (_| | |_| | | |\t\t   |\n\
-|\t\t |___/\__|_|  \___|_| |_|\__, |\__|_| |_|\t\t   |\n\
-|\t\t                          __/ |          \t\t   |\n\
-|\t\t                         |___/           \t\t   |\n\
-|--------------------------------------------------------------------------|"
+ASCII_ART1 = '''
+|--------------------------------------------------------------------------|\n
+|\t\t      _                        _   _     \t\t   |\n
+|\t\t     | |                      | | | |    \t\t   |\n
+|\t\t  ___| |_ _ __ ___ _ __   __ _| |_| |__  \t\t   |\n
+|\t\t / __| __| '__/ _ \ '_ \ / _` | __| '_ \ \t\t   |\n
+|\t\t \__ \ |_| | |  __/ | | | (_| | |_| | | |\t\t   |\n
+|\t\t |___/\__|_|  \___|_| |_|\__, |\__|_| |_|\t\t   |\n
+|\t\t                          __/ |          \t\t   |\n
+|\t\t                         |___/           \t\t   |\n
+|--------------------------------------------------------------------------|'''
+
+ASCII_ART = '''\
+|-------------------------------------------------------------------------|
+|\t___  ____ ____ ____ _ _ _ ____ ____ ___     ___ _ _  _ ____ \t  |
+|\t|__] |__| [__  [__  | | | |  | |__/ |  \     |  | |\/| |___ \t  |
+|\t|    |  | ___] ___] |_|_| |__| |  \ |__/     |  | |  | |___ \t  |
+|-------------------------------------------------------------------------|\n'''
+
 
 
 # Global Password Strength Requirements
@@ -55,14 +63,14 @@ class Password:
     def update(self, commonPasswords, commonWords):
         answer = commonPasswords.find(self.password)
         if answer != -1:
-            self.strength = "Found in list of most common passwords"
+            self.strength = "Common Pass"
             self.score = -1
             
         answer = 0
         
         answer = commonWords.find(self.password)
         if answer != -1:
-            self.strength = "Found in list of common words"
+            self.strength = "Common Word"
             self.score = -1
             
         
@@ -70,7 +78,9 @@ class Password:
             self.strength = "Too long"
             self.score = -1
         
-        if self.score != -1:
+        if self.score == -1:
+            self.score = 0
+        else:
             if len(self.password) >= 14:
                 self.length = 1
             
@@ -98,26 +108,25 @@ class Password:
             self.score = self.length + self.uppers + self.lowers + self.symbol + self.number
             
             if 0 <= self.score <= 2:
-                self.strength = "Very Weak"
+                self.strength = "Very Weak  "
             elif 3 <= self.score <= 4:
-                self.strength = "Weak"
+                self.strength = "Weak       "
             elif 5 <= self.score <= 6:
-                self.strength = "Moderate"
+                self.strength = "Moderate   "
             elif 7 <= self.score <= 8:
-                self.strength = "Secure"
+                self.strength = "Secure     "
             elif self.score == 9:
                 self.strength = "Very Secure"
 
 
 def main():
+    # Handle parameters
     parser = argparse.ArgumentParser(description="Checks the strength of a given list of passwords",
                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)    
     parser.add_argument("-i", "--input_file", help="location of the input file")
-    parser.add_argument("-o", "--output_file", default="stdout", help="location of the output file")
+    parser.add_argument("-o", "--output_file", help="location of the output file")
     parser.add_argument("-v", "--verbosity", default="2", type=int, choices=range(1,4), help="how detailed the output should be (1-3)")
     args = vars(parser.parse_args())
-    
-    passwordList = []
     
     # Input Arguments
     inputFile = args["input_file"]
@@ -134,15 +143,6 @@ def main():
             print("File", inputFile, "not found")
             return 0
     
-    # # Check if output file exists
-    # if outputFile != "stdout":
-    #     try:
-    #         f = open(outputFile, "r")
-                
-    #     except FileNotFoundError:
-    #         print("File", outputFile, "not found")
-    #         return 0
-    
     # Print fancy jazz
     print(ASCII_ART)
     
@@ -154,10 +154,13 @@ def main():
     wordF = open("10k_words.txt", "r", encoding="utf-8")
     commonWords = wordF.read()
     wordF.close()
+    
+    passwordList = []
 
     # Grab passwords from given location
     try:
         if inputFile == None:
+            print("> Enter a list of passwords, with with each password on its own line and\n> an empty line to finish.")
             inF = sys.stdin
         else:
             inF = open(inputFile, "r")
@@ -172,7 +175,6 @@ def main():
                 break
                                     
             temp.update(commonPasswords, commonWords)  
-                # checkStrength(temp, commonPasswords, commonWords)  
                                                     
             passwordList.append(temp)
                     
@@ -190,34 +192,47 @@ def main():
     
     # return 0
     
-    # if outputFile == "stdout":
-    #     for p in passwordList:
-    #         # checkStrength(p, commonPasswords, commonWords)
-            
-    #         print("Password:", p.password, end="\t")
-    #         print("Score:", p.score)
-    #         print()
-    
-    # else:
     try:
         if outputFile == None:
             outF = sys.stdout
         else:
             outF = open(outputFile, "w")
-                
-        lines = []
+        
+        if verbosity == 1:
+            outF.write("Strength\tPassword\n")
+            outF.write("------------------------\n")
             
-        for p in passwordList:
-            # checkStrength(p, commonPasswords, commonWords)                
-            lines.append("Password: " + p.password + "\tScore:" + str(p.score) + "\n")
+            for p in passwordList:
+                outF.write(p.strength + "\t" + p.password + "\n")
+                            
+        
+            
+                
+        elif verbosity == 2:
+            outF.write("Score (Max 9)\tStrength\tPassword\n")
+            outF.write("--------------------------------\n")
+            
+            for p in passwordList:
+                outF.write(str(p.score) + "\t\t" + p.strength + "\t" + p.password + "\n")
+                
+        else:
+            outF.write("Length\t# Uppercase\t# Lowercase\t# Symbols\t# Numbers\tScore (Max 9)\tStrength\tPassword\n")
+            outF.write("----------------------------------------------------------------------------------------------------------------\n")
+            
+            for p in passwordList:
+                outF.write(str(p.length) + "\t" + str(p.uppers) + "\t\t" + \
+                    str(p.lowers) + "\t\t" + str(p.symbol) + "\t\t" + str(p.number) + "\t\t" + \
+                    str(p.score) + "\t\t" + p.strength + "\t" + p.password + "\n")
                                         
-        outF.writelines(lines)
         outF.close()
             
-        print("Written to file -", outputFile)
+        if outputFile != None:
+            print("Written to file -", outputFile)
             
-    except:
-        print("Error writing to output file -", outputFile)
+    except Exception as error: 
+            print(error)
+    # except:
+    #     print("Error writing to output file -", outputFile)
 
 
 if (__name__ == "__main__"):
