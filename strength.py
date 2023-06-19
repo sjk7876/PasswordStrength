780e432b9ebadf403a757ebbcf938316ecde94de
Spencer <sjk7876@rit.edu>
2023-06-19 16:34:03 -0400

    Fixed some things before verbosity

--- strength.py
+++ strength.py
@@ -50,25 +50,24 @@ class Password:
         self.lowers = 0
         self.symbol = 0
         self.number = 0
-        self.note = ""
         self.strength = ""
         
     def update(self, commonPasswords, commonWords):
         answer = commonPasswords.find(self.password)
         if answer != -1:
-            self.note = "Password found in list of most common passwords"
+            self.strength = "Found in list of most common passwords"
             self.score = -1
             
         answer = 0
         
         answer = commonWords.find(self.password)
         if answer != -1:
-            self.note = "Password found in list of common words"
+            self.strength = "Found in list of common words"
             self.score = -1
             
         
         if len(self.password) > 1024:
-            self.note = "Password too long"
+            self.strength = "Too long"
             self.score = -1
         
         if self.score != -1:
@@ -76,7 +75,7 @@ class Password:
                 self.length = 1
             
             for letter in self.password:
-                if letter in string.punctuation and p.symbol < NUM_SYMBOLS:
+                if letter in string.punctuation and self.symbol < NUM_SYMBOLS:
                     self.symbol = self.symbol + 1
                     continue
                     
@@ -108,58 +107,6 @@ class Password:
                 self.strength = "Secure"
             elif self.score == 9:
                 self.strength = "Very Secure"
-
-
-def checkStrength(p, commonPasswords, commonWords):
-    answer = commonPasswords.find(p.password)
-    if answer != -1:
-        p.note = "Password found in list of most common passwords"
-        return -1
-        
-    answer = 0
-    
-    answer = commonWords.find(p.password)
-    if answer != -1:
-        p.note = "Password found in list of common words"
-        return -1
-        
-    
-    if len(p.password) > 1024:
-        p.note = "Password too long"
-        return -1
-    
-    elif len(p.password) >= 14:
-        p.length = 1
-      
-    for letter in p.password:
-        if letter in string.punctuation and p.symbol < NUM_SYMBOLS:
-            p.symbol = p.symbol + 1
-            continue
-            
-        if letter.isalpha:
-            if letter.isdigit():
-                if p.number < NUM_NUMBERS:
-                    p.number = p.number + 1
-                continue
-            
-            if letter.isupper():
-                if p.uppers < NUM_UPPER:
-                    p.uppers = p.uppers + 1
-                continue
-            
-            if letter.islower():
-                if p.lowers < NUM_LOWER:
-                    p.lowers = p.lowers + 1
-                continue
-    
-    p.score = p.length + p.uppers + p.lowers + p.symbol + p.number
-            
-    # print("score:", score)
-    # print("length:", length)
-    # print("upper:", uppers)
-    # print("lower:", lowers)
-    # print("symbol:", symbol)
-    # print("number:", number)
 
 
 def main():
@@ -209,98 +156,68 @@ def main():
     wordF.close()
 
     # Grab passwords from given location
-    # if inputFile == "stdin":
-    #     print("Enter in a newline separated list of passwords, ending with a blank line:")
-    #     while True:
-    #         t = input()
+    try:
+        if inputFile == None:
+            inF = sys.stdin
+        else:
+            inF = open(inputFile, "r")
+                
+        line = inF.readline()
+                
+        while line != "":
+            temp = Password("")                
+            temp.password = line.rstrip()
+                                    
+            if temp.password == "":
+                break
+                                    
+            temp.update(commonPasswords, commonWords)  
+                # checkStrength(temp, commonPasswords, commonWords)  
+                                                    
+            passwordList.append(temp)
+                    
+            line = inF.readline()
+                
+        inF.close()
+                
+    # except Exception as error: 
+    #         print(error)
+    except: 
+        print("Error reading input file -", inputFile)
+    
+    # for p in passwordList:
+    #     print(p.password, p.score)
+    
+    # return 0
+    
+    # if outputFile == "stdout":
+    #     for p in passwordList:
+    #         # checkStrength(p, commonPasswords, commonWords)
             
-    #         if t == "":
-    #             break
-            
-    #         temp = Password("")
-    #         temp.password = t.rstrip()
-            
-    #         checkStrength(temp, commonPasswords, commonWords)  
-            
-    #         passwordList.append(temp)
+    #         print("Password:", p.password, end="\t")
+    #         print("Score:", p.score)
+    #         print()
     
     # else:
-    #     try:
-    #         inF = open(inputFile, "r")
-    #         inputLines = inF.readlines()
-            
-    #         for line in inputLines:
-    #             temp = Password("")
-    #             temp.password = line.rstrip()
-                
-    #             checkStrength(temp, commonPasswords, commonWords)  
-                
-    #             passwordList.append(temp)
-            
-    #         inF.close()
-            
-    #     except:
-    #         print("Error reading input file -", inputFile)
-            
     try:
-            if inputFile == None:
-                inF = sys.stdin
-            else:
-                inF = open(inputFile, "r")
-            
-            line = inF.readline()
-            
-            while line != "":
-                temp = Password("")                
-                temp.password = line.rstrip()
-                                
-                if temp.password == "":
-                    break;
-                                
-                temp.update(commonPasswords, commonWords)  
-                # checkStrength(temp, commonPasswords, commonWords)  
-                                
-                passwordList.append(temp)
-                
-                line = inF.readline()
-            
-            inF.close()
-            
-    except Exception as error: 
-            print(error)
-    # except: 
-    #         print("Error reading input file -", inputFile)
-    
-    for p in passwordList:
-        print(p.password, p.score)
-    
-    return 0
-    
-    if outputFile == "stdout":
-        for p in passwordList:
-            # checkStrength(p, commonPasswords, commonWords)
-            
-            print("Password:", p.password, end="\t")
-            print("Score:", p.score)
-            print()
-    
-    else:
-        try:
+        if outputFile == None:
+            outF = sys.stdout
+        else:
             outF = open(outputFile, "w")
+                
+        lines = []
             
-            lines = []
+        for p in passwordList:
+            # checkStrength(p, commonPasswords, commonWords)                
+            lines.append("Password: " + p.password + "\tScore:" + str(p.score) + "\n")
+                                        
+        outF.writelines(lines)
+        outF.close()
             
-            for p in passwordList:
-                # checkStrength(p, commonPasswords, commonWords)                
-                lines.append("Password: " + p.password + "\tScore:" + str(p.score) + "\n")
-                            
-            outF.writelines(lines)
-            outF.close()
+        print("Written to file -", outputFile)
             
-            print("Written to file -", outputFile)
-            
-        except:
-            print("Error writing to output file -", outputFile)
+    except:
+        print("Error writing to output file -", outputFile)
 
 
 if (__name__ == "__main__"):
